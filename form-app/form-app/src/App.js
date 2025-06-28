bac58bc0643987d611b8d4021f696b1c2b7365e2
hinahoK <ttc2350st0007@edu.tech.ac.jp>
2025-06-28 11:17:13 +0900

    追加要件２

--- form-app/form-app/src/App.js
+++ form-app/form-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 function App() {
-  const [formData, setInputText] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     comment: "",
   });
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
+    setFormData({ ...formData, [e.target.name]: e.target.value.slice(0, 50) }); // 50文字制限
   };
 
   return (
@@ -18,15 +18,13 @@ function App() {
       {/* フォーム部分 */}
       <div>
         <input type="text" name="name" placeholder="名前" value={formData.name} onChange={handleChange} />
-        <input type="email" name="email" placeholder="メール" value={formData.email} onChange={handleChange} />
-        <textarea name="comment" placeholder="コメント" value={formData.comment} onChange={handleChange} />
-      </div>
+        <p>{formData.name ? `${formData.name.length}/50` : "未入力です"}</p>
 
-      {/* 表示部分 */}
-      <div>
-        <p>名前：{formData.name}</p>
-        <p>メールアドレス：{formData.email}</p>
-        <p>コメント：{formData.comment}</p>
+        <input type="email" name="email" placeholder="メール" value={formData.email} onChange={handleChange} />
+        <p>{formData.email ? `${formData.email.length}/50` : "未入力です"}</p>
+
+        <textarea name="comment" placeholder="コメント" value={formData.comment} onChange={handleChange} />
+        <p>{formData.comment ? `${formData.comment.length}/50` : "未入力です"}</p>
       </div>
     </div>
   );
