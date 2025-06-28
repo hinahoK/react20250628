6948070f7bb7f73c43f2c94377df084d43bc530f
hinahoK <ttc2350st0007@edu.tech.ac.jp>
2025-06-28 11:28:17 +0900

    mui

---
+++ MUI/src/MaterialBasic.js
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react';
+import {Button} from '@mui/material';
+
+export default function MaterialBasic(){
+    const font = css`
+    text-transform: none;
+    `;
+    return (
+        <>
+        <Button variant='text' css={font}>Text</Button>
+        <Button variant='text' color='secondary'>Text</Button>
+        <Button variant='contained' color='secondary'>Contained</Button>
+        <Button variant='outlined' color='secondary'>Outlined</Button>
+        </>
+    );
+}
